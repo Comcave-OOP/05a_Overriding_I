ebfbf5bd1dff89730168748db1d92fcd443a1515
Marinus <marinus.fehling@web.de>
2023-02-03 12:31:18 +0100

    Child, Parent, GrandParent, Vererbung

--- com/cc/java/Parent.java
+++ com/cc/java/Parent.java
@@ -1,10 +1,10 @@
 package com.cc.java;
 
-public class Parent {
+public class Parent extends GrandParent {
     
-    public String sayHello(){
-        return "Hello from Parent";
-    }
+    // public String sayHello(){
+    //     return "Hello from Parent";
+    // }
 
     public String sayHello(String name){
         return "Hello " + name + "!";
